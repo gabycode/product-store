9f9c7c1a85d50dda144f02bafac06ed33aa34b7e
gabycode <86427446+gabycode@users.noreply.github.com>
2024-12-07 12:43:52 -0400

    add: connectDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
@@ -7,5 +10,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, () => {
+  connectDB();
   console.log("Server is running at http://localhost:3000");
 });
