580e585d504c1c4a1c7be8a2d098f28f22535fe5
gabycode <86427446+gabycode@users.noreply.github.com>
2024-12-13 11:17:20 -0400

    add: post products

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.js";
 
 dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+app.post("/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
 });
 
 app.listen(3000, () => {
