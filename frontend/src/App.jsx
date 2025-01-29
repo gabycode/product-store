cb5b7dfb9869893c7506de19e824ae86268fea9d
gabycode <86427446+gabycode@users.noreply.github.com>
2025-01-29 18:20:40 -0400

    add: theme provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import Navbar from "./components/Navbar";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
 
 function App() {
   return (
-    <div className="h-screen">
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
+  );
+}
+
+function AppContent() {
+  const { darkMode } = useTheme();
+  return (
+    <div className={`h-screen ${darkMode ? "dark" : ""}`}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
