cea5c0163494c1eda50d1f3e7d875209c0d7cfb8
gabycode <86427446+gabycode@users.noreply.github.com>
2025-01-29 18:18:41 -0400

    add: themeContext

---
+++ frontend/src/context/ThemeContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
