d869d6e47dd40872f6d66c36dd077f03e040eba0
gabycode <86427446+gabycode@users.noreply.github.com>
2025-01-31 11:03:27 -0400

    add: homePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div className="z-0 min-h-full  dark:bg-gray-700">HomePage</div>;
 };
 
 export default HomePage;
