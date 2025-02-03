09d05130edc141040b0c32d6f40c84b4e0c31cb2
gabycode <86427446+gabycode@users.noreply.github.com>
2025-02-03 15:53:14 -0400

    add: notifications

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { useProductStore } from "../store/product";
+import {
+  AddProductSuccessNotif,
+  AddProductErrorNotif,
+} from "../components/notifications";
 
 const CreatePage = () => {
   const [newProduct, setNewProduct] = useState({
@@ -6,11 +11,28 @@ const CreatePage = () => {
     price: "",
     image: "",
   });
+
+  const [showSuccessNotif, setShowSuccessNotif] = useState(false);
+  const [showErrorNotif, setShowErrorNotif] = useState(false);
+
+  const { createProduct } = useProductStore();
+
+  const handleAddProduct = async () => {
+    const { success } = await createProduct(newProduct);
+    if (success) {
+      setShowSuccessNotif(true);
+    } else {
+      setShowErrorNotif(true);
+    }
+
+    setNewProduct({ name: "", price: "", image: "" });
+  };
+
   return (
-    <div className="min-h-screen px-8 py-24 mx-auto dark:bg-gray-900">
-      <div>
-        <form className="flex flex-col gap-4 max-w-2xl mx-auto">
-          <h1 className=" text-black text-xl font-bold mb-4 dark:text-white">
+    <div className="h-screen dark:bg-gray-900">
+      <div className="flex flex-col justify-center max-w-2xl mx-auto h-full ">
+        <div className="flex flex-col gap-4 p-8 bg-white rounded-lg shadow-lg dark:bg-gray-800 ">
+          <h1 className="text-black text-xl font-bold mb-4 dark:text-white">
             Create new product
           </h1>
           <div className="flex flex-col">
@@ -18,6 +40,10 @@ const CreatePage = () => {
               Name
             </label>
             <input
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
               type="text"
               placeholder="Enter product name"
               className="py-2 px-3 font-medium border border-gray-300 text-sm rounded dark:bg-gray-800 dark:text-white dark:border-gray-700"
@@ -28,6 +54,10 @@ const CreatePage = () => {
               Price
             </label>
             <input
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
               type="text"
               placeholder="Enter product price"
               className="py-2 px-3 font-medium border border-gray-300 text-sm rounded dark:bg-gray-800 dark:text-white dark:border-gray-700"
@@ -38,18 +68,31 @@ const CreatePage = () => {
               Image
             </label>
             <input
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
               type="text"
               placeholder="Enter image URL"
               className="py-2 px-3 font-medium border border-gray-300 text-sm rounded dark:bg-gray-800 dark:text-white dark:border-gray-700"
             />
           </div>
           <button
+            onClick={handleAddProduct}
             type="submit"
-            className="p-2 bg-black font-semibold text-white rounded dark:bg-white dark:text-black">
+            className="p-2 bg-black font-semibold text-white rounded-full dark:bg-gray-700 ">
             Create product
           </button>
-        </form>
+        </div>
       </div>
+      <AddProductSuccessNotif
+        show={showSuccessNotif}
+        onClose={() => setShowSuccessNotif(false)}
+      />
+      <AddProductErrorNotif
+        show={showErrorNotif}
+        onClose={() => setShowErrorNotif(false)}
+      />
     </div>
   );
 };
