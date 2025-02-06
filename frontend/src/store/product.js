51a1782fdb01ea7f51d5e6eea67c8e5d6f6f1a33
gabycode <86427446+gabycode@users.noreply.github.com>
2025-02-06 16:59:42 -0400

    add: deleteProducts function

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -30,4 +30,16 @@ export const useProductStore = create((set) => ({
       console.log("Failed to fetch products", error);
     }
   },
+  deleteProduct: async (id) => {
+    const res = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (!data.success) return { success: false, message: data.message };
+
+    set((state) => ({
+      products: state.products.filter((product) => product._id !== id),
+    }));
+    return { success: true, message: "Product deleted successfully" };
+  },
 }));
