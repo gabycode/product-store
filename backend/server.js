b55e5746771182a6d0667ba318d8df3b2b2a3589
gabycode <86427446+gabycode@users.noreply.github.com>
2025-02-08 19:50:32 -0400

    add: path for dirname

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.routes.js";
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
+const __dirname = path.resolve();
 app.use(express.json());
 const PORT = process.env.PORT;
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
