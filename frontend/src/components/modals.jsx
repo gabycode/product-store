e3ffe630d279e1481fff43da0a24f3aad5a04ce8
gabycode <86427446+gabycode@users.noreply.github.com>
2025-02-07 23:52:31 -0400

    add: modals

---
+++ frontend/src/components/modals.jsx
@@ -0,0 +1,138 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import {
+  CheckIcon,
+  ExclamationTriangleIcon,
+} from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+import { useTheme } from "../context/ThemeContext";
+
+export function ProductCreatedModal() {
+  const [open, setOpen] = useState(true);
+  const { darkMode } = useTheme();
+
+  return (
+    <Dialog
+      open={open}
+      onClose={setOpen}
+      className={`relative z-10 ${darkMode ? "dark" : ""}`}>
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+      />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95 dark:bg-gray-900 ">
+            <div>
+              <div className="mx-auto flex size-12 items-center justify-center rounded-full bg-green-100">
+                <CheckIcon
+                  aria-hidden="true"
+                  className="size-6 text-green-600"
+                />
+              </div>
+              <div className="mt-3 text-center sm:mt-5">
+                <DialogTitle
+                  as="h3"
+                  className="text-base font-semibold text-gray-900 dark:text-white">
+                  Product created!
+                </DialogTitle>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Thank you for creating a new product. Your product has been
+                    successfully created.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 sm:mt-6">
+              <Link
+                to="/"
+                onClick={() => setOpen(false)}
+                className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 ">
+                Go back to products
+              </Link>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
+
+export function ConfirmProductDeleteModal({
+  showModal,
+  setShowModal,
+  handleDeleteProduct,
+  productIdToDelete,
+}) {
+  const [open, setOpen] = useState(true);
+  const { darkMode } = useTheme();
+
+  return (
+    <Dialog
+      open={open}
+      onClose={setOpen}
+      className={`relative z-10 ${darkMode ? "dark" : ""}`}>
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+      />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95 dark:bg-gray-900 ">
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
+                <ExclamationTriangleIcon
+                  aria-hidden="true"
+                  className="size-6 text-red-600"
+                />
+              </div>
+              <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                <DialogTitle
+                  as="h3"
+                  className="text-base font-semibold text-gray-900 dark:text-white">
+                  Delete product
+                </DialogTitle>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Are you sure you want to delete this product? This action
+                    cannot be undone.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+              <button
+                type="button"
+                onClick={() => {
+                  handleDeleteProduct(productIdToDelete);
+                  setOpen(false);
+                }}
+                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">
+                Delete
+              </button>
+              <button
+                type="button"
+                data-autofocus
+                onClick={() => setOpen(false)}
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">
+                Cancel
+              </button>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
