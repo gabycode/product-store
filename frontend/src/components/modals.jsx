5353017fde200569a58e1b01597f8007d797373d
gabycode <86427446+gabycode@users.noreply.github.com>
2025-02-08 00:11:50 -0400

    add: dark mode ui

--- frontend/src/components/modals.jsx
+++ frontend/src/components/modals.jsx
@@ -45,7 +45,7 @@ export function ProductCreatedModal() {
                   Product created!
                 </DialogTitle>
                 <div className="mt-2">
-                  <p className="text-sm text-gray-500">
+                  <p className="text-sm text-gray-500 dark:text-gray-400">
                     Thank you for creating a new product. Your product has been
                     successfully created.
                   </p>
@@ -68,18 +68,17 @@ export function ProductCreatedModal() {
 }
 
 export function ConfirmProductDeleteModal({
-  showModal,
-  setShowModal,
+  open,
+  setOpen,
   handleDeleteProduct,
   productIdToDelete,
 }) {
-  const [open, setOpen] = useState(true);
   const { darkMode } = useTheme();
 
   return (
     <Dialog
       open={open}
-      onClose={setOpen}
+      onClose={() => setOpen(false)}
       className={`relative z-10 ${darkMode ? "dark" : ""}`}>
       <DialogBackdrop
         transition
@@ -105,7 +104,7 @@ export function ConfirmProductDeleteModal({
                   Delete product
                 </DialogTitle>
                 <div className="mt-2">
-                  <p className="text-sm text-gray-500">
+                  <p className="text-sm text-gray-500 dark:text-gray-400">
                     Are you sure you want to delete this product? This action
                     cannot be undone.
                   </p>
@@ -117,7 +116,7 @@ export function ConfirmProductDeleteModal({
                 type="button"
                 onClick={() => {
                   handleDeleteProduct(productIdToDelete);
-                  setOpen(false);
+                  setOpen(!open);
                 }}
                 className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">
                 Delete
@@ -125,7 +124,7 @@ export function ConfirmProductDeleteModal({
               <button
                 type="button"
                 data-autofocus
-                onClick={() => setOpen(false)}
+                onClick={() => setOpen(!open)}
                 className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">
                 Cancel
               </button>
